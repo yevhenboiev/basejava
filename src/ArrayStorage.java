bc80a17d497f2f39c806e683896cb73a92479813
yevhenboiev <yevhenboiev@gmail.com>
2021-05-26 13:29:02 +0300

    remake Resume

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -16,26 +16,25 @@ public class ArrayStorage {
         size++;
     }
 
-    String get(String uuid) {
+    Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                return storage[i].uuid;
+                return storage[i];
             }
         }
-
-        return "Данного uuid не найдено!";
+        return null;
     }
 
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if(storage[i].uuid.equals(uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
-                for(int j = i; j < size - 1; j++) {
+                size--;
+                for (int j = i; j < size - 1; j++) {
                     storage[j] = storage[j + 1];
                 }
             }
         }
-        size--;
     }
 
     /**
