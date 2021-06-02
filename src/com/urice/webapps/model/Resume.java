ade3bc6c44a1de0b022513ace38c95ed3f7e934c
yevhenboiev <yevhenboiev@gmail.com>
2021-06-02 21:39:34 +0300

    HW2

---
+++ src/com/urice/webapps/model/Resume.java
@@ -0,0 +1,22 @@
+package com.urice.webapps.model;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
