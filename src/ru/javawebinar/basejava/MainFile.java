018eaac2b26c0363871704e2fbf7141034876b7e
yevhenboiev@gmail.com <zipzone12345>
2021-07-08 19:29:16 +0300

    HW_8: make full path

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,27 +1,29 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) throws RuntimeException {
-
-        File file = new File(".//.gitignore");
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
+//        File file = new File(".//.gitignore");
+//        try {
+//            System.out.println(file.getCanonicalPath());
+//        } catch (IOException e) {
+//            throw new RuntimeException("Error", e);
+//        }
         File dir = new File("/home/java-developer/Документы/basejava/src/ru/javawebinar/basejava");
-        for (String name : dir.list()) {
-            System.out.println(name);
-        }
-
-        try (FileInputStream fil = new FileInputStream(file)) {
-            System.out.println(fil.read());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
+        if (dir.isDirectory()) {
+            for (File files : dir.listFiles()) {
+                if (files.isDirectory()) {
+                    System.out.println(files.getName());
+                    for (File filesDirectory : files.listFiles()) {
+                        System.out.println("\t" + filesDirectory.getName());
+                    }
+                } else {
+                    System.out.println(files.getName());
+                }
+            }
+        } else {
+            System.out.println(dir.getName());
         }
     }
 }
