987ddab337028ee6c885480e86eec2dcf728feed
yevhenboiev@gmail.com <zipzone12345>
2021-07-12 21:36:18 +0300

    HW_9: update MainFile

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -7,16 +7,16 @@ public class MainFile {
     public static void main(String[] args) throws RuntimeException {
         File dir = new File("./src/ru/javawebinar/basejava");
         MainFile mainFile = new MainFile();
-        mainFile.recourseMethod(dir);
+        mainFile.recourseMethod(dir, "");
     }
 
-    public void recourseMethod(File directory) {
+    public void recourseMethod(File directory, String str) {
         for (File files : Objects.requireNonNull(directory.listFiles())) {
-            if (files.isDirectory()) {
-                System.out.println("It's directory: " + files.getName());
-                recourseMethod(files);
-            } else if (files.isFile()) {
-                System.out.println("It's file: " + files.getName());
+            if (files.isFile()) {
+                System.out.println(str + "File: " + files.getName());
+            } else if (files.isDirectory()) {
+                System.out.println("Directory: " + files.getName());
+                recourseMethod(files, "    ");
             }
         }
     }
