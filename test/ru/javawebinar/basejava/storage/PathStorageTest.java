9946edbfd2db15ae08816b58356923b2b6cc5ee1
yevhenboiev@gmail.com <zipzone12345>
2021-07-15 14:26:13 +0300

    HW_9: remake STORAGE_DIR for getAbsolutePath()

--- test/ru/javawebinar/basejava/storage/PathStorageTest.java
+++ test/ru/javawebinar/basejava/storage/PathStorageTest.java
@@ -2,7 +2,7 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.storage.strategy.ObjectStreamStrategy;
 
-public class PathStorageTest extends AbstractStorageTest {
+public class PathStorageTest extends AbstractStorageTest) {
 
     public PathStorageTest() {
         super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new ObjectStreamStrategy()));
