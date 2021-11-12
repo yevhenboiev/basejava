d3951bf1c7de04fa76adb3c26431f1013e641579
yevhenboiev@gmail.com <zipzone12345>
2021-11-12 13:04:48 +0300

    HW_15: make table in ResumeServlet

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     @Override
@@ -12,8 +13,31 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF8");
         response.setCharacterEncoding("UTF8");
         response.setContentType("text/html; charset=UTF8");
-        request.getRequestDispatcher("./tableAllResumes.html").include(request, response);
+        Writer writer = response.getWriter();
+        writer.write(
+                    "<html>\n" +
+                        "<style>\n" +
+                            "table, th, td {\n" +
+                            "border:1px solid black;\n"+
+                            "}" +
+                        "</style>\n" +
+                        "<body> \n" +
+                            "<h2>Resumes Table</h2>\n" +
+                            "<table style=\"width:100 % \">\n" +
+                                "<tr>\n" +
+                                    "<th> UUID </th>\n" +
+                                    "<th> Full Name </th>\n" +
+                                "</tr>\n" +
+                                "<tr>\n" +
+                                    "<td> TODO Send uuid</td >\n" +
+                                    "<td> TODO Send Full_Name</td >" +
+                                "</tr>\n" +
+                            "</table>\n" +
+                        "</body>\n" +
+                        "</html>\n"
+        );
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
